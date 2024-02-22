439189550f102fd377b4ecb3ba8b36d6dab8606f
Vladyk144 <gmo3i@proton.me>
2024-02-22 15:25:57 +0200

    Add my kernel to build hope it works

--- ExSource.sh
+++ ExSource.sh
@@ -16,13 +16,16 @@ echo "$json"
 LINEAGE_URL=$(echo "$json" | jq -r '.["LineageOS-20"].kernel[0]' | cut -d ' ' -f 3)
 ZENITSU_URL=$(echo "$json" | jq -r '.Zenitsu.kernel[0] | split(" ")[2]')
 DIVEST_URL=$(echo "$json" | jq -r '.["DivestOS-13"].kernel[0]' | cut -d ' ' -f 3)
+LAVA_URL=$(echo "$json" | jq -r '.["vlad-experimental"].kernel[0]' | cut -d ' ' -f 3)
 
 # Print extracted URLs
 echo "Lineage URL: $LINEAGE_URL"
 echo "Zenitsu URL: $ZENITSU_URL"
 echo "Divest URL: $DIVEST_URL"
+echo "Divest URL: $LAVA_URL"
 
 # Set the extracted URLs as output parameters using the new method
 echo "lineage_url=$LINEAGE_URL" >> $GITHUB_ENV
 echo "zenitsu_url=$ZENITSU_URL" >> $GITHUB_ENV
 echo "divest_url=$DIVEST_URL" >> $GITHUB_ENV
+echo "lava_url=$lava_URL" >> $GITHUB_ENV
